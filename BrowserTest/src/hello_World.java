81d88feb2454985aa16bec4950a75eebee6e2d01
Alom <zohirul789@yahoo.com>
2018-07-28 15:10:58 -0400

    commited

--- BrowserTest/src/hello_World.java
+++ BrowserTest/src/hello_World.java
@@ -4,8 +4,20 @@ public class hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+		int count =0;
+		while(true) {
+			count++;
+			if(count==10) {
+				break;}
+			else
+			{
+				continue;
+			}
+		}
+		System.out.println(count);
 
 	}
-
 }
+
+
